b14796400b82b6768e490a7c810f2337d673cf85
Dongjin Lee <djlee1509@gmail.com>
2019-08-15 11:21:25 +0200

    Yelp MVP using Rails half-finished.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant1 = Restaurant.create(name: "Hello Fresh", address: "Here", category: "belgian")
+restaurant2 = Restaurant.create(name: "Franco Manca", address: "London", category: "italian")
